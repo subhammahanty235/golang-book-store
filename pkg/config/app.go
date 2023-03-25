38cadcc94c3039fc9c7de0e9ef9c3cb000533630
subhammahanty235 <subhammahanty235@gmail.com>
2023-03-25 21:58:05 +0530

    #1

---
+++ pkg/config/app.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "admin:admin/bookstoreproject?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		panic("err")
+	}
+
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
